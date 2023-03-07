758c878da429112fd4b532dd72e3d2537d1be44f
April Herwig <aprillherwig@gmail.com>
2023-03-07 17:55:48 +0100

    using DiffEqFlux

--- scripts/neural_chua.jl
+++ scripts/neural_chua.jl
@@ -1,73 +1,101 @@
-using StaticArrays, Random, ProgressMeter
-using OrdinaryDiffEq, SciMLSensitivity, Flux, CUDA
-using ChaoticNDETools, NODEData
+using Lux, Optimization, OptimizationOptimisers, Zygote, OrdinaryDiffEq, 
+      CUDA, SciMLSensitivity, Random, ComponentArrays, StaticArrays
+using Lux: setup, relu, SkipConnection
 using GAIO
+import DiffEqFlux: NeuralODE 
 
-Random.seed!(1234)
-include("./scripts/plotting.jl")
+#rng for Lux.setup
+rng = Random.seed!(234)
+include("plotting.jl")
 
 # Chua's circuit
-function v(u, p, t)
+function v(du, u, p, t)
     x, y, z = u
     a, b, m0, m1 = p
-    SA{Float32}[ a*(y-m0*x-m1/3.0*x^3), x-y+z, -b*y ]
+    du[1] = a*(y-m0*x-m1/3.0*x^3)
+    du[2] = x-y+z
+    du[3] = -b*y
+    return
 end
 
-p_ode = SA{Float32}[ 18.0, 33.0, -0.2, 0.01 ]
-v(u) = v(u, p_ode, 0)
+p_ode = NTuple{4,Float32}((18.0, 33.0, -0.2, 0.01))
 
-tspan = (0f0, 50f0)
+tspan = (0f0, 2f0)
 dt = 0.1f0
 
 a, b, m0, m1 = p_ode
-eq = SA{Float32}[ sqrt(-3*m0/m1), 0, -sqrt(-3*m0/m1) ]   # equilibrium
-x0 = SA{Float32}[ 5, 1, 10 ]
+eq = Float32[ sqrt(-3*m0/m1), 0, -sqrt(-3*m0/m1) ] |> gpu   # equilibrium
+x0 = Float32[ 0.6, 0.2, -5 ] |> gpu 
 #δ = 5f0
 #x0 = eq .+ Tuple(2δ * rand(Float32, 3) .- δ)               # start at a perturbed equilibrium
 
 prob = ODEProblem(v, x0, tspan, p_ode)
-true_sol = solve(prob, Tsit5(), saveat=dt, sensealg=InterpolatingAdjoint())
-train, valid = NODEDataloader(true_sol, 10; dt=dt, valid_set=0.8f0, GPU=true)
+true_sol = solve(prob, RadauIIA3(), saveat=dt)#=, sensealg=InterpolatingAdjoint())=# |> gpu
 
-N_weights = 10
+plot_trajectory(true_sol)
+
+N_weights = 6
 neural_net = Chain(
     Dense(3 => N_weights, relu),
-    SkipConnection(Dense(N_weights => N_weights, relu), +),
-    SkipConnection(Dense(N_weights => N_weights, relu), +),
+    Dense(N_weights => N_weights, relu),
+    #SkipConnection(Dense(N_weights => N_weights, relu), +),
     #SkipConnection(Dense(N_weights => N_weights, relu), +),
     Dense(N_weights => 3, relu)
-) |> gpu
+)
 
-p, re_nn = Flux.destructure(neural_net)
-neural_ode(u, p, t) = re_nn(p)(u)
-neural_ode_prob = ODEProblem(neural_ode, x0, tspan, p)
-model = ChaoticNDE(neural_ode_prob, alg=Tsit5(), sensealg=InterpolatingAdjoint())
+p, st = setup(rng, neural_net) 
+p = p |> ComponentArray |> gpu
+p .+= 0.3 .* CUDA.rand(Float32, size(p))
+p .*= -1
+st = st |> gpu
 
-loss(x, y) = sum(abs2, x - y)
+prob_neuralode = NeuralODE(neural_net |> gpu, (tspan[1], tspan[2]+eps(Float32)), RadauIIA3(), saveat=dt)
 
-η = 1e-3
-opt = Flux.AdamW(η)
-opt_state = Flux.setup(opt, model)
-
-TRAIN = true
-if TRAIN
-
-    @showprogress "Training..." for i_e = 1:100
-
-        Flux.train!(model, train, opt_state) do m, t, x
-            result = m((t,x))
-            loss(result, x)
-        end 
-
-        #plot_nde(sol, model, train)
-
-        if i_e % 30 == 0
-            η /= 2
-            Flux.adjust!(opt_state, η)
-        end
-    end
-
+function predict_neuralode(p)
+  prob_neuralode(x0,p,st) |> first |> gpu
 end
+
+function loss_neuralode(p)
+    pred = predict_neuralode(p)
+    loss = sum(abs2, true_sol .- pred)
+    return loss, pred
+end
+
+η = 1f-1
+optalg = Adam(η)
+adtype = Optimization.AutoZygote()
+optf = Optimization.OptimizationFunction((p,_)->loss_neuralode(p), adtype)
+optprob = Optimization.OptimizationProblem(optf, p)
+p = Optimization.solve(
+    optprob,
+    optalg,
+    callback = callback,
+    maxiters = 5
+)
+
+η = 1f-4
+optalg = Momentum(η)#Adam(η)
+adtype = Optimization.AutoZygote()
+optf = Optimization.OptimizationFunction((p,_)->loss_neuralode(p), adtype)
+optprob = Optimization.OptimizationProblem(optf, p)
+p = Optimization.solve(
+    optprob,
+    optalg,
+    callback = callback,
+    maxiters = 50
+)
+
+# Use result params to create a single-step map
+v_trained(x) = neural_net(x, result.u, st)
+f_trained(x) = rk4_flow_map(v_trained, x, 0.1f0, 1)
+
+function v_true(u, p=p_ode, t=0)
+    x, y, z = u
+    a, b, m0, m1 = p
+    SA[ a*(y-m0*x-m1/3.0*x^3), x-y+z, -b*y ]
+end
+f_true(x) = rk4_flow_map(v_true, x, 0.1f0, 1)
+
 
 # -------------------------------------
 
@@ -76,24 +104,19 @@ Q = Box{Float32}(center, radius)
 P = BoxPartition(Q, (128,128,128))
 
 no_of_points = 200
-
-f(x) = rk4_flow_map(v, x, 0.01f0, 10)
-F = BoxMap(:montecarlo, f, P, no_of_points=no_of_points)
-
-trained_neural_ode = re_nn(model.p)
-f_trained(x) = rk4_flow_map(trained_neural_ode, x, 0.01f0, 10)
 F_trained = BoxMap(:montecarlo, f_trained, P, no_of_points=no_of_points)
+F_true = BoxMap(:montecarlo, f_true, P, no_of_points=no_of_points)
 
 # computing the attractor by covering the 2d unstable manifold of two equilibria
 S = cover(P, [eq, -eq])
 
-W = unstable_set(F, S)
+W = unstable_set(F_true, S)
 W_trained = unstable_set(F_trained, S)
 
 plot_unstable_sets(W, W_trained)
 plot_symdiff(W, W_trained)
 
-T = TransferOperator(F, W, W)
+T = TransferOperator(F_true, W, W)
 # we give Arpack some help converging to the eigenvalues,
 # see the Arpack docs for explanations of keywords
 tol, maxiter, v0 = eps()^(1/4), 1000, ones(Float32, size(T, 2))
--- scripts/plotting.jl
+++ scripts/plotting.jl
@@ -1,23 +1,50 @@
-using GLMakie: plot, plot!, Figure, Axis3, Colorbar
+using WGLMakie: plot, plot!, lines, lines!, Legend, Figure, Axis3, Colorbar
+
+function plot_trajectory(sol)
+    fig = Figure();
+    ax = Axis3(fig[1,1], aspect=(1,1.2,1), azimuth=pi/10)
+    ms = lines!(ax, Array(sol[1, :]), Array(sol[2, :]), Array(sol[3, :]))
+    fig
+end
+
+function callback(p, l, pred; doplot = true)
+    if doplot
+        fig = Figure();
+        ax = Axis3(fig[1,1], aspect=(1,1.2,1), azimuth=pi/10)
+        ms1 = lines!(ax, Array(true_sol[1, :]), Array(true_sol[2, :]), Array(true_sol[3, :]), label="truth")
+        ms2 = lines!(ax, Array(pred[1, :]), Array(pred[2, :]), Array(pred[3, :]), label="prediction")
+        Legend(fig[1,2], ax)
+        display(fig)
+    end
+    println("Loss: ", l)
+    return false
+end
 
 function plot_nde(sol, model, train)
-    fig, ax, ms = plot(sol.t, Array(model((sol.t,train[1][2])))', label="Neural ODE")
-    plot!(ax, sol.t, Array(sol)', label="Training Data")
+    pred = Array(model((sol.t,train[1][2])))
+    tr = Array(sol)
+    fig, ax, ms = lines(sol.t, pred[1, :], label="Neural ODE dim 1")
+    lines!(ax, sol.t, pred[2, :], label="Neural ODE dim 2")
+    lines!(ax, sol.t, pred[3, :], label="Neural ODE dim 3")
+    lines!(ax, sol.t, tr[1, :], label="Training Data dim 1")
+    lines!(ax, sol.t, tr[2, :], label="Training Data dim 2")
+    lines!(ax, sol.t, tr[3, :], label="Training Data dim 3")
+    Legend(fig[1,2], ax)
     fig
 end
 
 function plot_unstable_sets(W, W_trained)
     fig = Figure();
-    ax = Axis3(fig[1,1], aspect=(1,1.2,1))
+    ax = Axis3(fig[1,1], aspect=(1,1.2,1), azimuth=pi/10)
     plot!(ax, W, color=(:blue, 0.4), label="True unstable manifold")
-    ax2 = Axis3(fig[1,2], aspect=(1,1.2,1))
+    ax2 = Axis3(fig[1,2], aspect=(1,1.2,1), azimuth=pi/10)
     plot!(ax2, W_trained, color=(:red, 0.4), label="Predicted unstable manifold")
     fig
 end
 
 function plot_symdiff(W, W_trained)
     fig = Figure();
-    ax = Axis3(fig[1,1], aspect=(1,1.2,1))
+    ax = Axis3(fig[1,1], aspect=(1,1.2,1), azimuth=pi/10)
     plot!(ax, setdiff(W, W_trained), color=(:blue, 0.8))
     plot!(ax, setdiff(W_trained, W), color=(:red, 0.8))
     fig
@@ -27,9 +54,9 @@ function plot_measures(μ, μ_trained)
     vals = [values(μ); values(μ_trained)]
     colorrange = (minimum(vals), maximum(vals))
     fig = Figure();
-    ax = Axis3(fig[1,1], aspect=(1,1.2,1))
+    ax = Axis3(fig[1,1], aspect=(1,1.2,1), azimuth=pi/10)
     ms = plot!(ax, μ, colormap=(:jet, 0.4), colorrange=colorrange, label="True almost invariant sets")
-    ax2 = Axis3(fig[1,2], aspect=(1,1.2,1))
+    ax2 = Axis3(fig[1,2], aspect=(1,1.2,1), azimuth=pi/10)
     plot!(ax2, μ_trained, colormap=(:jet, 0.4), colorrange=colorrange, label="Predicted almost invariant sets")
     Colorbar(fig[1, 3], ms)
     fig
