41e722dfcc2fc9b4af743a80264df6bd7f40f238
April Herwig <aprillherwig@gmail.com>
2023-03-13 13:41:38 +0100

    implement changes from meeting

--- scripts/neural_chua.jl
+++ scripts/neural_chua.jl
@@ -19,7 +19,7 @@ v(u) = v(u, p_ode, 0)
 λ_max = 12.5
 t0, t1 = 0f0, 1f-1
 tspan = (t0, t1)
-dt = 0.1f0
+dt = 1f-2
 
 a, b, m0, m1 = p_ode
 eq = SA{Float32}[ sqrt(-3*m0/m1), 0, -sqrt(-3*m0/m1) ]   # equilibrium
@@ -27,33 +27,35 @@ x0 = SA{Float32}[ -4.9, 7.6, 0.5 ]
 #δ = 5f0
 #x0 = eq .+ Tuple(2δ * rand(Float32, 3) .- δ)               # start at a perturbed equilibrium
 
-prob = ODEProblem(v, x0, (t0, 500*t1), p_ode)
+prob = ODEProblem(v, x0, (t0, 150*t1), p_ode)
 true_sol = solve(prob, Tsit5(), saveat=dt, sensealg=InterpolatingAdjoint())
 
 plot_trajectory(true_sol)
 
-train, valid = NODEDataloader(true_sol, 4; dt=dt, valid_set=0.8, GPU=true)
+train, valid = NODEDataloader(true_sol, 4; dt=dt, valid_set=0.8, GPU=false#=true=#)
 
-N_weights = 30
+N_weights = 10
 neural_net = Chain(
-    u -> [u; u.^3],
-    Dense(6 => N_weights, swish),
+    #BatchNorm(1),
+    #u -> 2*u,
+    #u -> [u; u.^3],
+    Dense(3 => N_weights, swish),
     #Dense(N_weights => N_weights, swish), 
     #Dense(N_weights => N_weights, swish), 
+    #SkipConnection(Dense(N_weights => N_weights, swish), +),
     SkipConnection(Dense(N_weights => N_weights, swish), +),
-    SkipConnection(Dense(N_weights => N_weights, swish), +),
-    Dense(N_weights => 3, swish)
-) |> gpu
+    Dense(N_weights => 3)
+) #|> gpu
 
 p, re_nn = Flux.destructure(neural_net)
-p = p |> gpu
+p = p #|> gpu
 neural_ode(u, p, t) = re_nn(p)(u)
-neural_ode_prob = ODEProblem(neural_ode, CuArray(x0), tspan, p)
-model = ChaoticNDE(neural_ode_prob, alg=Tsit5(), gpu=true, sensealg=InterpolatingAdjoint())
+neural_ode_prob = ODEProblem(neural_ode, #=CuArray(x0)=#x0, tspan, p)
+model = ChaoticNDE(neural_ode_prob, alg=Tsit5(), gpu=false#=true=#, sensealg=InterpolatingAdjoint())
 
 loss(x, y) = sum(abs2, x - y)
 
-η = 1e-4
+η = 1e-3
 θ = 1f-4
 opt = Flux.OptimiserChain(Flux.WeightDecay(θ), Flux.RMSProp(η))
 opt_state = Flux.setup(opt, model) 
@@ -68,6 +70,7 @@ grad = Flux.gradient(model) do m
     loss(result, train[1][2])
 end   
 
+#=
 iter = 0
 while isapprox(norm(grad[1].p), 0, atol=100) && iter < 4
     model.p .= 0.01 .* CUDA.rand(Float32, size(p))
@@ -77,6 +80,7 @@ while isapprox(norm(grad[1].p), 0, atol=100) && iter < 4
     end    
     iter += 1
 end
+=#
 
 TRAIN = true
 epochs = 30
@@ -105,13 +109,7 @@ end
 p_trained = Array(model.p)
 BSON.@save "params_$( round(now(), Minute(1)) )" p_trained
 
-v_trained = re_nn(CuArray(p_trained))
-
-function v_true(u, p=p_ode, t=0)
-    x, y, z = u
-    a, b, m0, m1 = p
-    SA[ a*(y-m0*x-m1/3.0*x^3), x-y+z, -b*y ]
-end
+v_trained = re_nn(#=CuArray(p_trained)=#p_trained)
 
 # -------------------------------------
 
